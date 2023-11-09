bdb235ee9a2c980852c6192cf722fc570cc57a35
Renat Shageev <renat.shageev@king.com>
2023-11-09 09:50:03 +0000

    Day 1, Year 2022

--- CodeAdvent2023.cpp
+++ CodeAdvent2023.cpp
@@ -1,9 +1,9 @@
 import utils;
 
-import AoC_2021.Day1;
+import AoC_2022.Day1;
 
 int main()
 {
-    std::cout << "Part 1: " << Solve_1("2021/input_1.txt") << '\n';
-    std::cout << "Part 2: " << Solve_2("2021/input_1.txt") << '\n';
+    std::cout << "Part 1: " << Solve_1("2022/input_1.txt") << '\n';
+    std::cout << "Part 2: " << Solve_2("2022/input_1.txt") << '\n';
 }
