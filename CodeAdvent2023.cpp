557b8888d89fac7930b235740a96752e28420e39
Renat Shageev <shageev.renat@gmail.com>
2023-12-21 16:11:33 +0000

    [2023] Day 21 solution

--- CodeAdvent2023.cpp
+++ CodeAdvent2023.cpp
@@ -1,11 +1,11 @@
-import AoC_2023.Day20;
+import AoC_2023.Day21;
 
 import std;
 
 int main()
 {
-    const auto res1 = Solve_1("2023/input_20.txt");
-    const auto res2 = Solve_2("2023/input_20.txt");
+    const auto res1 = Solve_1("2023/input_21.txt");
+    const auto res2 = Solve_2("2023/input_21.txt");
 
     std::cout << std::format("\nPart 1: {}\n", res1);
     std::cout << std::format("\nPart 2: {}\n", res2);
