68764c69fa861388001566557219668ff577f26e
Renat Shageev <shageev.renat@gmail.com>
2024-12-11 06:24:21 +0000

    Print out execution time for all solutions

--- CodeAdvent.cpp
+++ CodeAdvent.cpp
@@ -6,11 +6,15 @@ import std;
 int main()
 {
     const auto res1t = Solve_1("test.txt");
-    const auto res1 = Solve_1("input.txt");
     const auto res2t = Solve_2("test.txt");
-    const auto res2 = Solve_2("input.txt");
 
-    std::cout << std::format("\nPart 1: test {}, answer {}\n", res1t, res1);
-    std::cout << std::format("\nPart 2: test {}, answer {}\n", res2t, res2);
+    Timer timer;
+    const auto res1 = Solve_1("input.txt");
+    const auto time1 = timer.Get();
+    const auto res2 = Solve_2("input.txt");
+    const auto time2 = timer.Get();
+
+    std::cout << std::format("\nPart 1: test {}, answer {}, time {}\n", res1t, res1, time1);
+    std::cout << std::format("\nPart 2: test {}, answer {}, time {}\n", res2t, res2, time2);
     return 0;
 }
